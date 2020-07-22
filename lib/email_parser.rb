8149bf7ceff1450fe228607e13c094f2330ac139
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-07-22 23:18:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,6 +5,10 @@
 class EmailAddressParser
   attr_accessor :email_addresses
 
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+  
   def parse
     @email_addresses.split(/[,\s]+/)
   end
